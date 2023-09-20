459334c2dab9904cabf5bc8f1e402510ea7ea055
cristian harea <cristian.harea@gmail.com>
2023-09-20 21:21:08 +0300

    added byte padding feature

--- aes.h
+++ aes.h
@@ -1,6 +1,9 @@
 #ifndef AES_H
 #define AES_H
 
+#define TEXT_SIZE (18)
+#define PADDED_TEXT_SIZE (TEXT_SIZE) + ((16 - ((TEXT_SIZE) % 16)) % 16)
+
 enum errorCode
 {
     SUCCESS = 0,
@@ -22,7 +25,7 @@ enum expKeySize
 {
     EXP_SIZE_16 = 176,
     EXP_SIZE_24 = 208,
-    EXP_SIZE_32 = 254
+    EXP_SIZE_32 = 240
 };
 
 extern void expandKey(unsigned char *expandedKey, unsigned char *key, enum keySize, unsigned char expandedKeySize);
--- main.c
+++ main.c
@@ -2,30 +2,47 @@
 #include <stdlib.h> 
 #include "aes.h"
 
+static void add_padding(unsigned char* text, unsigned char* padded_text, int size)
+{
+    int desiredPadding = (16 - (size % 16)) % 16;
+
+    for (int i = 0; i < size + desiredPadding; i++)
+    {
+        if(i >= size)
+        {
+            *(padded_text + i) = 0;
+        }
+        else
+        {
+            *(padded_text + i) = *(text + i);
+        }
+    } 
+    
+}
+
 int main(int argc, char *argv[])
 {
     // the expanded keySize
-    int expandedKeySize = EXP_SIZE_32;
+    int expandedKeySize = EXP_SIZE_16;
 
     // the expanded key
     unsigned char expandedKey[expandedKeySize];
 
     // the cipher key
-    //unsigned char key[16] = {0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14};
+    unsigned char key[16] = {0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14};
     //unsigned char key[24] = {0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c};
-    unsigned char key[32] = {0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24};
+    //unsigned char key[32] = {0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24};
 
     // the cipher key size
-    enum keySize size = SIZE_32;
+    enum keySize size = SIZE_16;
 
     // the plaintext
-    unsigned char plaintext[16] = {'a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-
+    unsigned char plaintext[TEXT_SIZE] = {'a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'p', 'o'};
+    unsigned char paddedtext[PADDED_TEXT_SIZE]; 
     // the ciphertext
-    unsigned char ciphertext[16];
-
+    unsigned char ciphertext[PADDED_TEXT_SIZE];
     // the decrypted text
-    unsigned char decryptedtext[16];
+    unsigned char decryptedtext[PADDED_TEXT_SIZE];
 
     int i;
 
@@ -35,7 +52,7 @@ int main(int argc, char *argv[])
 
     printf("\nCipher Key (HEX format):\n");
 
-    for (i = 0; i < SIZE_32; i++)
+    for (i = 0; i < SIZE_16; i++)
     {
         // Print characters in HEX format, 16 chars per line
         printf("%2.2x%c", key[i], ((i + 1) % 16) ? ' ' : '\n');
@@ -53,27 +70,43 @@ int main(int argc, char *argv[])
 
     printf("\nPlaintext (HEX format):\n");
 
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < TEXT_SIZE; i++)
     {
         printf("%2.2x%c", plaintext[i], ((i + 1) % 16) ? ' ' : '\n');
     }
 
+    //add byte padding to the text
+    add_padding(plaintext, paddedtext, TEXT_SIZE);
+
+    printf("\n\nPadded text (HEX format):\n");
+
+    for (i = 0; i < PADDED_TEXT_SIZE; i++)
+    {
+        printf("%2.2x%c", paddedtext[i], ((i + 1) % 16) ? ' ' : '\n');
+    }
+
     // AES Encryption
-    aes_encrypt(plaintext, ciphertext, key, SIZE_32);
+    for (i = 0; i < PADDED_TEXT_SIZE; i += 16)
+    {
+        aes_encrypt(&paddedtext[i], &ciphertext[i], key, SIZE_16);
+    }
 
     printf("\nCiphertext (HEX format):\n");
 
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < PADDED_TEXT_SIZE; i++)
     {
         printf("%2.2x%c", ciphertext[i], ((i + 1) % 16) ? ' ' : '\n');
     }
 
     // AES Decryption
-    aes_decrypt(ciphertext, decryptedtext, key, SIZE_32);
+    for (i = 0; i < PADDED_TEXT_SIZE; i += 16)
+    {
+        aes_decrypt(&ciphertext[i], &decryptedtext[i], key, SIZE_16);
+    }
 
     printf("\nDecrypted text (HEX format):\n");
 
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < TEXT_SIZE; i++)
     {
         printf("%2.2x%c", decryptedtext[i], ((i + 1) % 16) ? ' ' : '\n');
     }
